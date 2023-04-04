78dd88408b2d3c451b30534e4e229ff939419917
vr1 <113930888+vr1eva@users.noreply.github.com>
2023-04-04 19:00:57 +0000

    Fetch cat photos

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,12 @@
 import Head from 'next/head';
 import images from "./images.json"
+import { CldImage } from 'next-cloudinary';
+import useSWR from 'swr';
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Dashboard() {
+  const { data, error } = useSWR('/api/staticdata', fetcher);
+
   return (
     <div>
       <Head>
@@ -9,11 +14,16 @@ export default function Dashboard() {
       </Head>
 
       <main className="container">
-        {images.map(url => (
+        {/* {images.map(url => (
           <p>{url}</p>
-        ))}
-      </main>
+        ))} */}
 
+        <CldImage
+          width="600"
+          height="600"
+          src="<Public ID>" />
+
+      </main>
     </div>
   );
 }
