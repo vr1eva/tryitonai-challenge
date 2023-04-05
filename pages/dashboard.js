3456c08e6d77b144cf991cbeaf7b6b2fd91688ee
vr1 <113930888+vr1eva@users.noreply.github.com>
2023-04-05 01:19:10 +0000

    move image list to separate component

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,35 +1,10 @@
 
-import useSWR from 'swr';
-import {useState} from "react"
-import { CldImage } from 'next-cloudinary';
-
+import Images from "./components/Images.js"
 
 export default function Dashboard() {
-  const [selectedImageId, setSelectedImageId] = useState(null);
-  const { data, error } = useSWR('/api/staticdata', async (url) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  });
-  const handleImageClick = (id) => {
-    setSelectedImageId(id);
-  };
-  
-  if (error) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
-  const parsed = JSON.parse(data)
-  console.log(parsed.images)
   return (
-    <div className="grid grid-cols-5 gap-4">
-     {parsed.images.map((imageId, index) => (
-        <CldImage
-        key={index}
-        className={`cursor-pointer rounded-lg object-cover h-full w-full`}
-        width="600"
-        height="600"
-        src={imageId}
-        onClick={() => handleImageClick(imageId)} />
-     ))}
-    </div>
+    <>
+    <Images/>
+    </>
   );
 }
